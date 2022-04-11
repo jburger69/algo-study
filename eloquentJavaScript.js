060e2459e65c482943c78c315be09823300a22fd
Joshua Burger <joshberger6969@outlook.com>
2022-04-10 21:57:30 -0400

    Function to count letters in string

--- eloquentJavaScript.js
+++ eloquentJavaScript.js
@@ -46,3 +46,23 @@ const min = (arg1, arg2) => {
 };
 
 console.log(min(100, 20000));
+
+//5
+// Bean counter.
+
+function countChar(string, ch) {
+    let counted = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] == ch) {
+        counted += 1;
+      }
+    }
+    return counted;
+}
+// recurvise call on countChar within countBs function.
+function countBs(string) {
+    return countChar(string, "B");
+}
+
+console.log(countBs("AAFFFFBBDDDDUUUNNNEBBSSJJJBSDBJSDFIIRBBC"));
+console.log(countChar("sdgsdgsdkkrtythyhgbkakksdfkkdfgerlak", "k"));
