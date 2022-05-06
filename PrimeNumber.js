2a92217004281220fb188844eafd89fbc36f0c0d
Joshua Burger <joshberger6969@outlook.com>
2022-05-06 14:49:19 -0400

    Prime Number

---
+++ PrimeNumber.js
@@ -0,0 +1,15 @@
+// Given a non-negative integer N check if it is a prime number
+
+// Prime numbers are positive numbers greater than 1 that are only divisible by the number 1 or itself.
+
+// Examples of prime numbers: 2, 3, 5, 7, 11, 13, 17, 19, 23
+
+// Input: N = 31
+// Output: true
+
+// Input: N = 6
+// Output: false
+// Explanation: 6 is divisible by 2 and 3.
+
+// O(n) - time
+// O(1) - space
